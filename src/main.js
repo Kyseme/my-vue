aeeeec5e956796c7e42c1c15ef72752fd45b01bb
kyseme <kyseme@126.com>
2019-08-06 22:37:38 +0800

    增加arr深度校验

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App.vue';
+import ArrDeep from './test/ArrDeep.vue';
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: h => h(ArrDeep),
 }).$mount('#app')
---
+++ src/test/ArrDeep.vue
@@ -0,0 +1,42 @@
+<template>
+<div>
+    <div v-for="(item,index) in obj" :key="item.name">
+            {{item.name}}
+        <div v-for="(temp,tempIdx) in item.suggest" :key="temp.id">
+            {{temp.name}}
+            <span @click="removeName(index,tempIdx)">删除</span>
+        </div>
+        <hr>
+        <hr>
+    </div>
+</div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            obj: [{
+                name: 'sqw',
+                suggest: [
+                    {name: 'one', id: '1111'}
+                ]
+            },
+                {
+                    name: 'sun',
+                    suggest: [
+                        {name: 'sun-one', id: '3333'},
+                        {name: 'sun-two', id: '222'}
+                    ]
+                }]
+        };
+    },
+    methods: {
+        removeName(index, tempIdx) {
+            this.obj[index].suggest.splice(tempIdx, 1);
+            // eslint-disable-next-line
+            console.log('objjjjj', this.obj[index].suggest);
+        }
+    }
+};
+</script>
+
