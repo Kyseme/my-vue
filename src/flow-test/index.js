85cac6c9c3fe8248a1a40016aa95462be39793e3
sunqianwen <sunqianwen@baidu.com>
2019-10-29 14:20:20 +0800

    msf

--- src/flow-test/index.js
+++ src/flow-test/index.js
@@ -3,3 +3,17 @@
 var a:number = 3;
  // eslint-disable-next-line
 console.log(a);
+
+
+
+// function add():void | string{
+//     return '';
+// }
+
+
+function add():any{
+
+}
+
+let tt = add();
+console.log(tt);
