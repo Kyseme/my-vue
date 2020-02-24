1ce5d498385724183766414d11b975f5e120ba6a
sunqianwen <sunqianwen@baidu.com>
2020-02-24 11:23:22 +0800

    chore(element-ui): 增加element-ui

--- src/main.js
+++ src/main.js
@@ -2,16 +2,26 @@ import Vue from 'vue'
 import App from './App.vue';
 // import Parent from './pages/Parent.vue';
 // import ArrDeep from './test/ArrDeep.vue';
-import Test from './pages/Test.vue';
+import Tables from './pages/Tables.vue';
 import VueRouter from 'vue-router';
 import {routes} from './router.js';
+import Element from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
 Vue.config.productionTip = false
 Vue.use(VueRouter);
-const router=new VueRouter({
+Vue.use(Element);
+const router= new VueRouter({
   routes
 });
 
+// new Vue({
+//   router,
+//   render: h => h(App),
+// }).$mount('#app');
+
+
 new Vue({
   router,
-  render: h => h(App),
+  render: h => h(Tables),
 }).$mount('#app');
